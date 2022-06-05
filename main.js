dd76cfe1b8261ad1938d0d43a245115ce0f3abb4
Arthur Lay <arthur.lay@gmail.com>
2022-06-05 00:06:05 -0400

    Variables added for player and computer scores but scores are not adding yet.

--- main.js
+++ main.js
@@ -4,28 +4,59 @@ function computerPlay (play) {
     return randomizedPlay;
 }
 
-var gameSelection = ['Rock', 'Paper', 'Scissors'];
+var gameSelection = ['rock', 'paper', 'scissors'];
+var playerScore = 0
+var computerScore = 0
 
-function aRound (playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+
+function playRound (playerSelection, computerSelection) {
+    
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You win! Rock beats Scissors!';
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return 'You win! Paper beats Rock!';
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerScore += 1;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         return 'You win! Scissors beats Paper!';
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        playerScore += 1;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         return 'You lose! Paper beats Rock!';
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        computerScore += 1;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         return 'You lose! Scissors beats Paper!';
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        computerScore += 1;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'You lose! Rock Beats Scissors';
-    } else if (playerSelection === "" && computerSelection === 'Rock' || 'Paper' || 'Scissors'){
-        return 'You lose! You did not make a move!';
-    } else {
+        computerScore += 1;
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
         return 'Draw!';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Draw!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {    
+        return 'Draw!';
+    } else {
+        return 'You did not make a move!'; 
+        computerScore +=1;
     }
 }
 
-var playerSelection = prompt("Rock, Paper, Scissors?!");
-var computerSelection = computerPlay(gameSelection);
-console.log(aRound(playerSelection, computerSelection));
+function game() { 
+    for (let i = 0; i < 5; i++) {
+        var playerSelection = prompt("Rock, Paper, Scissors?!").toLowerCase();
+        var computerSelection = computerPlay(gameSelection);
+        console.log(playRound(playerSelection, computerSelection));    
+    }
+}
+
+game()
+
+//if (playerScore = computerScore) {
+//    console.log('Draw!');
+//} else if (playerScore > computerScore) {
+//    console.log('You Win!')
+//} else if (playerScore < computerScore) {
+//    console.log('You Lose!')
+//}
+
+
