1c499908dc3d3fae1069d4c2b5cf2eed01b6c3d6
Arthur Lay <arthur.lay@gmail.com>
2022-06-05 16:44:46 -0400

    Scorekeeping now functioning. Game works in console and displays final score after 5 rounds.

--- main.js
+++ main.js
@@ -8,36 +8,35 @@ var gameSelection = ['rock', 'paper', 'scissors'];
 var playerScore = 0
 var computerScore = 0
 
-
 function playRound (playerSelection, computerSelection) {
     
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore += 1;
         return 'You win! Rock beats Scissors!';
-        playerScore += 1;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore += 1;
         return 'You win! Paper beats Rock!';
-        playerScore += 1;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You win! Scissors beats Paper!';
         playerScore += 1;
+        return 'You win! Scissors beats Paper!';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You lose! Paper beats Rock!';
-        computerScore += 1;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You lose! Scissors beats Paper!';
-        computerScore += 1;
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You lose! Rock Beats Scissors';
-        computerScore += 1;
-    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'Draw!';
-    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'Draw!';
-    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {    
-        return 'Draw!';
-    } else {
-        return 'You did not make a move!'; 
         computerScore +=1;
+        return 'You lose! Paper beats Rock!';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore +=1;
+        return 'You lose! Scissors beats Paper!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore +=1;
+        return 'You lose! Rock Beats Scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'Draw! Rock vs Rock!';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Draw! Paper vs Paper!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {    
+        return 'Draw! Scissors vs Scissors!';
+    } else {
+        computerScore +=1;
+        return 'You did not make a move!'; 
     }
 }
 
@@ -49,14 +48,15 @@ function game() {
     }
 }
 
+function scoreKeeping() {
+    if (playerScore === computerScore) {
+    console.log(`Draw! ${playerScore} - ${computerScore}`);
+    } else if (playerScore > computerScore) {
+    console.log(`You win! ${playerScore} - ${computerScore}`)
+    } else if (playerScore < computerScore) {
+    console.log(`You lose! ${playerScore} - ${computerScore}`)
+    }
+}
+
 game()
-
-//if (playerScore = computerScore) {
-//    console.log('Draw!');
-//} else if (playerScore > computerScore) {
-//    console.log('You Win!')
-//} else if (playerScore < computerScore) {
-//    console.log('You Lose!')
-//}
-
-
+scoreKeeping ()
